06ec362e3fc42177ec18314b3c90c0b080bd460a
Ravindranadh varma <74560627+ravindranadh-varma@users.noreply.github.com>
2021-04-09 21:56:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+add = document.getElementById("add");
+function getAndUpdate(){
+    tex = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+    if (localStorage.getItem("itemsJson") == null) {
+        itemsJsonArray = [];
+        itemsJsonArray.push([tex, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    } else {
+        itemsJsonArraystr = localStorage.getItem("itemsJson");
+        itemsJsonArray = JSON.parse(itemsJsonArraystr);
+        itemsJsonArray.push([tex, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    }
+    update();
+}
+function update(){
+    if (localStorage.getItem("itemsJson") == null) {
+        itemsJsonArray = [];
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    } else {
+        itemsJsonArraystr = localStorage.getItem("itemsJson");
+        itemsJsonArray = JSON.parse(itemsJsonArraystr);
+    }
+    let tablebody = document.getElementById("tablebody");
+    str = "";
+    itemsJsonArray.forEach((element, index) => {
+      str+=`
+      <tr>
+            <th scope="row">${index+1}</th>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td><button class="btn btn-sm btn-dark" onClick="deleted(${index})">Delete</button></td>
+          </tr>
+      `
+  
+    });
+    tablebody.innerHTML = str;
+}
+add.addEventListener("click", getAndUpdate);
+update();
+function deleted(itemIndex){
+    itemsJsonArraystr = localStorage.getItem("itemsJson");
+    itemsJsonArray = JSON.parse(itemsJsonArraystr);
+    itemsJsonArray.splice(itemIndex,1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    update();
+}
+function clearAll(){
+    if(confirm("Do you really want to clear?")){
+    localStorage.clear();
+    update();
+}
+}
+contactus=document.getElementById("contactUs");
+contactus.addEventListener("click",function(){
+    alert("Mail to example123@gmail.com")
+})
+search=document.getElementById("search");
+search.addEventListener("click",function(){
+    alert("serach in google mava bro");
+})
+ 
